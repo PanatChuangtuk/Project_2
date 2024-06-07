54a79ce216fdd1ac20f1c51fe0317fa7d3ea7c46
PanatChuangtuk <panat32@hotmail.com>
2024-06-07 13:39:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let weight = prompt('กรอกน้ำหนัก :');
-let height = prompt('กรอกส่วนสูง :');
+//let weight = prompt('กรอกน้ำหนัก :');
+//let height = prompt('กรอกส่วนสูง :');
+while (true) {
+  userInput = prompt('กรุณาเลือกเพศของคุณ :\n1.เพศชาย\n2.เพศหญิง\n');
+  if (userInput === '1') {
+    alert('เพศชาย');
+    break;
+  } else if (userInput === '2') {
+    alert('เพศหญิง');
+    break;
+  } else {
+    alert('คุณไม่ได้เลือกเพศกรุณาเลือกใหม่');
+  }
+}
