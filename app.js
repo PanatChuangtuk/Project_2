b220989bc5da41bc0e0a20a27997f2dc45cc674b
PanatChuangtuk <panat32@hotmail.com>
2024-06-07 13:06:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let age = prompt('กรุณากรอกอายุของคุณ');
 if (age > 13) {
-  document.getElementById('content').innerHTML = '';
+  document.getElementById('content').innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/ybvkdiwI83c?si=31J5RxfELg5UTU_b" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
