2c551bcdcf66304eecf2ba7dd32ef1479181a3d1
PanatChuangtuk <panat32@hotmail.com>
2024-06-07 14:33:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,44 +11,35 @@ while (true) {
   }
 }
 
-var w = prompt('กรุณากรอกน้ำหนักของคุณ (KG.)');
-var h = prompt('กรุณากรอกความสูงของคุณ (CM.)');
-if (w > 0 && h > 0) {
-  var bmi = w / (h / 100) ** 2;
-  (document.getElementById('W').innerHTML = 'น้ำหนักของคุณ คือ ' + w + ' KG.'),
-    '<br/>';
-  (document.getElementById('H').innerHTML = 'ความสูงของคุณ คือ ' + h + ' CM.'),
-    '<br/>';
-  document.getElementById('BM').innerHTML =
-    'ค่า BMI หรือค่าดัชนีมวลกาย คือ ' + bmi.toFixed(2); //toFixed กำหนดจำนวนทศนิยมที่ต้องการให้แสดงผล
-  if (bmi >= 30) {
-    // ส่วนเงื่อนไขของคนที่ BMI มากกว่าหรือเท่ากับ 30 ขึ้นไป
-    document.getElementById('result').innerHTML =
-      'คุณเป็นโรคอ้วนระดับ 3 (สูงสุด) กรุณาลดน้ำหนักโดยเร็ว!';
-  } else if (bmi <= 29.9) {
-    if (bmi >= 25) {
+while (true) {
+  var w = prompt('กรุณากรอกน้ำหนักของคุณ (KG.)');
+  var h = prompt('กรุณากรอกความสูงของคุณ (CM.)');
+  if (w > 0 && h > 0 && h != '' && w != '') {
+    var bmi = w / (h / 100) ** 2;
+    document.getElementById('W').innerHTML = 'น้ำหนักของคุณ คือ ' + w + ' KG.';
+    document.getElementById('H').innerHTML = 'ความสูงของคุณ คือ ' + h + ' CM.';
+    document.getElementById('BM').innerHTML =
+      'ค่า BMI หรือค่าดัชนีมวลกาย คือ ' + bmi.toFixed(2); //toFixed กำหนดจำนวนทศนิยมที่ต้องการให้แสดงผล
+    if (bmi >= 30) {
+      // ส่วนเงื่อนไขของคนที่ BMI มากกว่าหรือเท่ากับ 30 ขึ้นไป
+      document.getElementById('result').innerHTML =
+        'คุณเป็นโรคอ้วนระดับ 3 (สูงสุด) กรุณาลดน้ำหนักโดยเร็ว!';
+    } else if (bmi >= 25) {
+      // ส่วนเงื่อนไขของคนที่ BMI มากกว่าหรือเท่ากับ 25 ขึ้นไป
       document.getElementById('result').innerHTML = 'คุณเป็นโรคอ้วนระดับ 2';
-    } // ส่วนเงื่อนไขของคนที่ BMI ต่ำกว่า 29.90 แต่มากกว่าหรือเท่ากับ 25
-    else {
-      if (bmi <= 24.9) {
-        if (bmi >= 23) {
-          document.getElementById('result').innerHTML = 'คุณเป็นโรคอ้วนระดับ 1';
-        } // ส่วนเงื่อนไขของคนที่ BMI ต่ำกว่า 24.90 แต่มากกว่าหรือเท่ากับ 23
-        else {
-          if (bmi <= 22.9) {
-            if (bmi >= 18.6) {
-              document.getElementById('result').innerHTML = 'คุณเป็นคนสุขภาพดี';
-            } // ส่วนเงื่อนไขของคนที่ BMI ต่ำกว่า 22.90 แต่มากกว่าหรือเท่ากับ 18.50
-            else {
-              if (bmi <= 18.5) {
-                document.getElementById('result').innerHTML =
-                  'คุณเป็นผอมเกินไป กรุณาเพิ่มน้ำหนัก!';
-              } // ส่วนเงื่อนไขของคนที่ BMI ต่ำกว่าหรือเท่ากับ 18.50 ลงไป
-            }
-          }
-        }
-      }
+    } else if (bmi >= 23) {
+      // ส่วนเงื่อนไขของคนที่ BMI มากกว่าหรือเท่ากับ 23 ขึ้นไป
+      document.getElementById('result').innerHTML = 'คุณเป็นโรคอ้วนระดับ 1';
+    } else if (bmi >= 18.5) {
+      // ส่วนเงื่อนไขของคนที่ BMI มากกว่าหรือเท่ากับ 18.5 ขึ้นไป
+      document.getElementById('result').innerHTML = 'คุณเป็นคนสุขภาพดี';
+    } else {
+      // ส่วนเงื่อนไขของคนที่ BMI น้อยกว่า 18.5
+      document.getElementById('result').innerHTML =
+        'คุณเป็นผอมเกินไป กรุณาเพิ่มน้ำหนัก!';
     }
+    break;
+  } else {
+    alert('คุณกรอกข้อมูลไม่ครบ');
   }
 }
-//document.getElementById('totals').innerHTML;
